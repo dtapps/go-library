6019428268c320ec3537698cfc9e89308a6c3707
李光春 <gc@liguangchun.cn>
2021-06-07 10:12:10 +0800

    增加文件/文件夹函数

--- .codeeui.sh
+++ .codeeui.sh
@@ -1,13 +1,13 @@
-#使用vendor文件夹构建
+# 使用vendor文件夹构建
 export GO15VENDOREXPERIMENT=1
-export GOPROXY=https://goproxy.cn,direct
-##在工作目录创建源文件夹
+export GOPROXY=https://mirrors.aliyun.com/goproxy/
+# 在工作目录创建源文件夹
 mkdir -p $GOPATH/src/dtapps/golibrary/
-##拷贝代码到创建好的目录
+# 拷贝代码到创建好的目录
 cp -rf . $GOPATH/src/dtapps/golibrary/
-##进入项目
+# 进入项目
 cd $GOPATH/src/dtapps/golibrary/
-##构建，在GOPATH下生成构建包
+# 构建，在GOPATH下生成构建包
 go mod tidy
-##列出文件
+# 列出文件
 ls -lh
