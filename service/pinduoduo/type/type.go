e97ecb4dddb6577ae732fc7da17004a608ee4489
李光春 <gc@dtapp.net>
2021-07-27 13:09:28 +0800

    update *

--- service/pinduoduo/type/type.go
+++ service/pinduoduo/type/type.go
@@ -3,4 +3,5 @@ package _type
 const (
 	GoodsPidGenerate = "pdd.ddk.goods.pid.generate"
 	GoodsSearch      = "pdd.ddk.goods.search" // 多多进宝商品查询 https://jinbao.pinduoduo.com/third-party/api-detail?apiName=pdd.ddk.goods.search
+	GoodsDetail      = "pdd.ddk.goods.detail" // 多多进宝商品详情查询 https://jinbao.pinduoduo.com/third-party/api-detail?apiName=pdd.ddk.goods.detail
 )
