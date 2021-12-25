d35458d83117333ff898e22a547800f550279c67
李光春 <gc@dtapp.net>
2021-12-25 10:10:14 +0800

    update

---
+++ utils/gossh/gossh_test.go
@@ -0,0 +1,9 @@
+package gossh
+
+import (
+	"testing"
+)
+
+func TestClient(t *testing.T) {
+	//Tunnel("root", "", ":22", ":3306", "localhost:13306")
+}
--- utils/url/url_test.go
+++ utils/url/url_test.go
@@ -14,8 +14,3 @@ func TestDeCode(t *testing.T) {
 	u := "https%3A%2F%2Fwww.dtapp.net"
 	fmt.Println(DeCode(u))
 }
-
-func TestParseQuery(t *testing.T) {
-	u := "https://api.dtapp.net/v8/ip/qqwry?ip=113.118.170.199"
-	fmt.Println(ParseQuery(u))
-}
