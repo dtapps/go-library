f18d18d7a2784922066bf86d63311e7cc9f230e3
李光春 <gc@dtapp.net>
2021-12-30 16:13:08 +0800

    优化微信服务

--- service/wechatunion/cgi-bin.token.monitor.go
+++ service/wechatunion/cgi-bin.token.monitor.go
@@ -2,10 +2,6 @@ package wechatunion
 
 import (
 	"errors"
-	"fmt"
-	"gopkg.in/dtapps/go-library.v3/utils/goredis"
-	"gopkg.in/dtapps/go-library.v3/utils/gotime"
-	"time"
 )
 
 var (
@@ -21,32 +17,12 @@ func (app *App) AuthGetAccessTokenMonitor(qdType string) error {
 			if app.Db == nil {
 				return errors.New("驱动没有初始化")
 			}
-			token := app.AuthGetAccessToken()
-			if token.Result.AccessToken == "" {
-				return errors.New("获取AccessToken失败")
-			} else {
-				app.Db.Create(&WechatAccessTokenDbModel{
-					AppID:       app.AppId,
-					AppSecret:   app.AppSecret,
-					AccessToken: token.Result.AccessToken,
-					ExpiresIn:   token.Result.ExpiresIn,
-					ExpiresTime: gotime.Current().AfterSeconds(7000).Format(),
-				})
-				return nil
-			}
+			app.GetAccessTokenDb()
 		case QdTypeRdb:
 			if app.RDb == nil {
 				return errors.New("驱动没有初始化")
 			}
-			cacheName := fmt.Sprintf("wechat_access_token:%v", app.AppId)
-			redis := goredis.App{
-				Rdb: app.RDb,
-			}
-			token := app.AuthGetAccessToken()
-			if token.Result.AccessToken == "" {
-				return errors.New("获取AccessToken失败")
-			}
-			redis.NewStringOperation().Set(cacheName, token.Result.AccessToken, goredis.WithExpire(time.Second*7000))
+			app.GetAccessTokenRDb()
 			return nil
 		default:
 			return errors.New("驱动类型不在范围内")
