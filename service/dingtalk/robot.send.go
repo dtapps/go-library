efd325887385394f2d6d2efb33ae4057bad74a5a
李光春 <gc@dtapp.net>
2021-12-30 15:32:04 +0800

    优化服务

--- service/dingtalk/robot.send.go
+++ service/dingtalk/robot.send.go
@@ -1,24 +1,38 @@
 package dingtalk
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 )
 
-type RobotSendResult struct {
+type RobotSendResponse struct {
 	Errcode int64  `json:"errcode"`
 	Errmsg  string `json:"errmsg"`
 }
 
+type RobotSendResult struct {
+	Result RobotSendResponse // 结果
+	Body   []byte            // 内容
+	Err    error             // 错误
+}
+
+func NewRobotSendResult(result RobotSendResponse, body []byte, err error) *RobotSendResult {
+	return &RobotSendResult{Result: result, Body: body, Err: err}
+}
+
 // RobotSend 自定义机器人
 // https://open.dingtalk.com/document/group/custom-robot-access
-func (app *App) RobotSend(notMustParams ...Params) (body []byte, err error) {
+func (app *App) RobotSend(notMustParams ...Params) *RobotSendResult {
 	// 参数
 	params := app.NewParamsWith(notMustParams...)
 	// 时间
 	timestamp := time.Now().UnixNano() / 1e6
 	// 请求
-	body, err = app.request(fmt.Sprintf("https://oapi.dingtalk.com/robot/send?access_token=%s&timestamp=%d&sign=%s", app.AccessToken, timestamp, app.sign(timestamp)), params, http.MethodPost)
-	return body, err
+	body, err := app.request(fmt.Sprintf("https://oapi.dingtalk.com/robot/send?access_token=%s&timestamp=%d&sign=%s", app.AccessToken, timestamp, app.sign(timestamp)), params, http.MethodPost)
+	// 定义
+	var response RobotSendResponse
+	err = json.Unmarshal(body, &response)
+	return NewRobotSendResult(response, body, err)
 }
