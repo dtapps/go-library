4ad16ecb9d24dbdeb78c75bb71ae685f04b3729b
李光春 <gc@dtapp.net>
2021-12-30 17:05:35 +0800

    优化微信服务

--- service/wechatoffice/getticket.monitor.go
+++ service/wechatoffice/getticket.monitor.go
@@ -1,23 +1,17 @@
 package wechatoffice
 
-import (
-	"errors"
-)
+import "errors"
 
 // AuthGetJsapiTicketMonitor 监控api_ticket
 func (app *App) AuthGetJsapiTicketMonitor(qdType string) error {
-	result := app.GetCallBackIp()
-	if len(result.Result.IpList) <= 0 {
-		switch qdType {
-		case QdTypeDb:
-			app.GetJsapiTicketDb()
-			return nil
-		case QdTypeRdb:
-			app.GetJsapiTicketRDb()
-			return nil
-		default:
-			return errors.New("驱动类型不在范围内")
-		}
+	switch qdType {
+	case QdTypeDb:
+		app.GetJsapiTicketDb()
+		return nil
+	case QdTypeRdb:
+		app.GetJsapiTicketRDb()
+		return nil
+	default:
+		return errors.New("驱动类型不在范围内")
 	}
-	return nil
 }
