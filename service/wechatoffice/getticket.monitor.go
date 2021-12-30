46cb0a53e29b5ea0dda74562f088ba066a3ea50e
李光春 <gc@dtapp.net>
2021-12-30 17:14:26 +0800

    优化微信服务

--- service/wechatoffice/getticket.monitor.go
+++ service/wechatoffice/getticket.monitor.go
@@ -3,15 +3,21 @@ package wechatoffice
 import "errors"
 
 // AuthGetJsapiTicketMonitor 监控api_ticket
-func (app *App) AuthGetJsapiTicketMonitor(qdType string) error {
+func (app *App) AuthGetJsapiTicketMonitor(qdType string) (string, error) {
 	switch qdType {
 	case QdTypeDb:
-		app.GetJsapiTicketDb()
-		return nil
+		if app.Db == nil {
+			return "", errors.New("驱动没有初始化")
+		}
+		app.AccessToken = app.GetAccessTokenDb()
+		return app.GetJsapiTicketDb(), nil
 	case QdTypeRdb:
-		app.GetJsapiTicketRDb()
-		return nil
+		if app.RDb == nil {
+			return "", errors.New("驱动没有初始化")
+		}
+		app.AccessToken = app.GetAccessTokenRDb()
+		return app.GetJsapiTicketRDb(), nil
 	default:
-		return errors.New("驱动类型不在范围内")
+		return "", errors.New("驱动类型不在范围内")
 	}
 }
--- service/wechatunion/cgi-bin.token.monitor.go
+++ service/wechatunion/cgi-bin.token.monitor.go
@@ -9,24 +9,30 @@ var (
 	QdTypeRdb = "redis"
 )
 
-func (app *App) AuthGetAccessTokenMonitor(qdType string) error {
-	result := app.GetCallBackIp()
-	if len(result.Result.IpList) <= 0 {
-		switch qdType {
-		case QdTypeDb:
-			if app.Db == nil {
-				return errors.New("驱动没有初始化")
-			}
-			app.GetAccessTokenDb()
-		case QdTypeRdb:
-			if app.RDb == nil {
-				return errors.New("驱动没有初始化")
-			}
-			app.GetAccessTokenRDb()
-			return nil
-		default:
-			return errors.New("驱动类型不在范围内")
+func (app *App) AuthGetAccessTokenMonitor(qdType string) (string, error) {
+	switch qdType {
+	case QdTypeDb:
+		if app.Db == nil {
+			return "", errors.New("驱动没有初始化")
 		}
+		app.AccessToken = app.GetAccessTokenDb()
+		result := app.GetCallBackIp()
+		if len(result.Result.IpList) <= 0 {
+			return app.GetAccessTokenDb(), nil
+		}
+		return app.AccessToken, nil
+	case QdTypeRdb:
+		if app.RDb == nil {
+			return "", errors.New("驱动没有初始化")
+		}
+		app.AccessToken = app.GetAccessTokenRDb()
+		result := app.GetCallBackIp()
+		if len(result.Result.IpList) <= 0 {
+			return app.GetAccessTokenRDb(), nil
+		}
+		return app.AccessToken, nil
+	default:
+		return "", errors.New("驱动类型不在范围内")
 	}
-	return nil
+	return "", nil
 }
