9a2935a7eb547e193f7a3217e1c7eab908e0f82c
李光春 <gc@dtapp.net>
2022-06-26 11:28:32 +0800

    - update request

--- utils/gorequest/ip.go
+++ utils/gorequest/ip.go
@@ -10,35 +10,39 @@ import (
 // 解析 X-Real-IP 和 X-Forwarded-For 以便于反向代理（nginx 或 haproxy）可以正常工作。
 func ClientIp(r *http.Request) string {
 
+	// cloudflare
+	CfConnectingIp := strings.TrimSpace(r.Header.Get("Cf-Connecting-Ip"))
+	if CfConnectingIp != "" {
+		return CfConnectingIp
+	}
+
 	// 转发IP
-	xForwardedFor := r.Header.Get("X-Forwarded-For")
-	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
-	if ip != "" {
-		return ip
+	xForwardedFor := strings.TrimSpace(strings.Split(r.Header.Get("X-Forwarded-For"), ",")[0])
+	if xForwardedFor != "" {
+		return xForwardedFor
 	}
 
 	// 真实Ip
-	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
-	if ip != "" {
-		return ip
+	XRealIp := strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if XRealIp != "" {
+		return XRealIp
 	}
 
 	// HTTP客户端IP
-	httpClientIp := r.Header.Get("HTTP_CLIENT_IP")
-	ip = strings.TrimSpace(strings.Split(httpClientIp, ",")[0])
-	if ip != "" {
-		return ip
+	HttpClientIp := strings.TrimSpace(strings.Split(r.Header.Get("HTTP_CLIENT_IP"), ",")[0])
+	if HttpClientIp != "" {
+		return HttpClientIp
 	}
 
 	// HTTP转发IP
-	HttpXForwardedFor := r.Header.Get("HTTP_X_FORWARDED_FOR")
-	ip = strings.TrimSpace(strings.Split(HttpXForwardedFor, ",")[0])
-	if ip != "" {
-		return ip
+	HttpXForwardedFor := strings.TrimSpace(strings.Split(r.Header.Get("HTTP_X_FORWARDED_FOR"), ",")[0])
+	if HttpXForwardedFor != "" {
+		return HttpXForwardedFor
 	}
 
 	// 系统
-	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+	ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr))
+	if err == nil {
 		return ip
 	}
 
