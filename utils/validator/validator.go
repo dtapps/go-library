d86c84c74e608541823d6b0982c935fff6702d17
李光春 <gc@dtapp.net>
2022-06-30 11:03:25 +0800

    - update validator

--- utils/validator/validator.go
+++ utils/validator/validator.go
@@ -2,10 +2,12 @@ package validator
 
 import (
 	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/en"
 	"github.com/go-playground/locales/zh"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
-	zh_translations "github.com/go-playground/validator/v10/translations/zh"
+	enTranslations "github.com/go-playground/validator/v10/translations/en"
+	chTranslations "github.com/go-playground/validator/v10/translations/zh"
 )
 
 type Validator struct {
@@ -14,38 +16,52 @@ type Validator struct {
 	trans    ut.Translator
 }
 
-func NewValidator() (*Validator, error) {
+func NewValidator(local string) (*Validator, error) {
 
 	v := &Validator{}
 
-	// 注册翻译器
-	zhN := zh.New()
-	v.uni = ut.New(zhN, zhN)
-
-	v.trans, _ = v.uni.GetTranslator("zh")
-
 	// 获取gin的校验器
-	v.validate = binding.Validator.Engine().(*validator.Validate)
+	var ok bool
+	if v.validate, ok = binding.Validator.Engine().(*validator.Validate); ok {
 
-	// 注册翻译器
-	err := zh_translations.RegisterDefaultTranslations(v.validate, v.trans)
-	if err != nil {
-		return nil, err
+		zhT := zh.New() // 中文
+		enT := en.New() // 英文
+		v.uni = ut.New(enT, zhT, enT)
+
+		var o bool
+		v.trans, o = v.uni.GetTranslator(local)
+		if !o {
+			return nil, nil
+		}
+
+		// 注册翻译器
+		var err error
+		switch local {
+		case "en":
+			err = enTranslations.RegisterDefaultTranslations(v.validate, v.trans)
+		case "zh":
+			err = chTranslations.RegisterDefaultTranslations(v.validate, v.trans)
+		default:
+			err = chTranslations.RegisterDefaultTranslations(v.validate, v.trans)
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		return v, nil
 	}
-
-	return v, nil
+	return nil, nil
 }
 
 // Translate 翻译错误信息
 // https://learnku.com/articles/59498
-func (v *Validator) Translate(err error) map[string][]string {
-
-	var result = make(map[string][]string)
-
-	errors := err.(validator.ValidationErrors)
-
-	for _, err := range errors {
-		result[err.Field()] = append(result[err.Field()], err.Translate(v.trans))
+// https://www.cnblogs.com/silent-cxl/p/15181647.html
+func (v *Validator) Translate(err error) (errMsg string) {
+	errs := err.(validator.ValidationErrors)
+	for _, err := range errs {
+		errMsg = err.Translate(v.trans)
+		break
 	}
-	return result
+	return
 }
