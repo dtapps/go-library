d9fa5d9ed69ce1d89d7af2288c7a151fc7dc5a04
李光春 <gc@dtapp.net>
2022-07-01 09:44:40 +0800

    - update only

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	gitee.com/chunanyong/zorm v1.5.5
 	github.com/aliyun/aliyun-oss-go-sdk v2.2.4+incompatible
 	github.com/allegro/bigcache/v3 v3.0.2
-	github.com/aws/aws-sdk-go v1.44.45
+	github.com/aws/aws-sdk-go v1.44.46
 	github.com/baidubce/bce-sdk-go v0.9.126
 	github.com/basgys/goxml2json v1.1.0
 	github.com/beego/beego/v2 v2.0.4
@@ -103,11 +103,11 @@ require (
 	go.etcd.io/etcd/client/pkg/v3 v3.5.4 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/net v0.0.0-20220630215102-69896b714898 // indirect
 	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f // indirect
 	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
 	golang.org/x/time v0.0.0-20220609170525-579cf78fd858 // indirect
-	google.golang.org/genproto v0.0.0-20220628213854-d9e0b6570c03 // indirect
+	google.golang.org/genproto v0.0.0-20220630174209-ad1d48641aa7 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- utils/only/custom.go
+++ utils/only/custom.go
@@ -22,8 +22,16 @@ func GenerateId(customId string) string {
 	if err == nil {
 		return three
 	}
-	four, err := generateIdFour(customId)
-	return four
+	four, err := generateIdFour(customId, currentTime)
+	if err == nil {
+		return four
+	}
+	five, err := generateIdFive(customId, currentTime)
+	if err == nil {
+		return five
+	}
+	six, err := generateIdSix(customId)
+	return six
 }
 
 // GenerateIdAndTime 生成18一编号
@@ -40,8 +48,16 @@ func GenerateIdAndTime(customId, customTime string) string {
 	if err == nil {
 		return three
 	}
-	four, err := generateIdFour(customId)
-	return four
+	four, err := generateIdFour(customId, customTime)
+	if err == nil {
+		return four
+	}
+	five, err := generateIdFive(customId, customTime)
+	if err == nil {
+		return five
+	}
+	six, err := generateIdSix(customId)
+	return six
 }
 
 // 生成18位时间[年月日时分]唯一编号
@@ -125,8 +141,62 @@ func generateIdThree(customId, setTime string) (string, error) {
 	return fmt.Sprintf("%v%s%s", customId, gotime.SetCurrentParse(setTime).SetFormat(dateFormat), gorandom.Numeric(randomLength)), nil
 }
 
+// 生成18位时间[年月]唯一编号
+func generateIdFour(customId, setTime string) (string, error) {
+
+	var (
+		randomLength   = 4             // 随机数据长度
+		customIdLength = len(customId) // 自定义长度
+	)
+
+	const (
+		dataLength       = 18              // 默认数据长度
+		dateFormat       = "200601"        // 时间格式
+		dateFormatLength = len(dateFormat) // 时间格式长度
+	)
+
+	// 重新计算随机数据长度
+	randomLength = dataLength - (dateFormatLength + customIdLength)
+
+	if dateFormatLength+customIdLength > dataLength {
+		return "", errors.New("超出长度")
+	} else if dateFormatLength+customIdLength == dataLength {
+		return "", errors.New("无法使用时间方法")
+	} else if randomLength < 3 {
+		return "", errors.New("自定义的数据过长")
+	}
+	return fmt.Sprintf("%v%s%s", customId, gotime.SetCurrentParse(setTime).SetFormat(dateFormat), gorandom.Numeric(randomLength)), nil
+}
+
+// 生成18位时间[年]唯一编号
+func generateIdFive(customId, setTime string) (string, error) {
+
+	var (
+		randomLength   = 4             // 随机数据长度
+		customIdLength = len(customId) // 自定义长度
+	)
+
+	const (
+		dataLength       = 18              // 默认数据长度
+		dateFormat       = "2006"          // 时间格式
+		dateFormatLength = len(dateFormat) // 时间格式长度
+	)
+
+	// 重新计算随机数据长度
+	randomLength = dataLength - (dateFormatLength + customIdLength)
+
+	if dateFormatLength+customIdLength > dataLength {
+		return "", errors.New("超出长度")
+	} else if dateFormatLength+customIdLength == dataLength {
+		return "", errors.New("无法使用时间方法")
+	} else if randomLength < 3 {
+		return "", errors.New("自定义的数据过长")
+	}
+	return fmt.Sprintf("%v%s%s", customId, gotime.SetCurrentParse(setTime).SetFormat(dateFormat), gorandom.Numeric(randomLength)), nil
+}
+
 // 生成18位随机唯一编号
-func generateIdFour(customId string) (string, error) {
+func generateIdSix(customId string) (string, error) {
 
 	var (
 		randomLength   = 4             // 随机数据长度
