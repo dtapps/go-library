070243ebab26214eaf6d5ac9adf97a6196137bc5
李光春 <gc@dtapp.net>
2022-07-06 14:45:47 +0800

    - update wechatpayopen

--- service/wechatpayopen/set.go
+++ service/wechatpayopen/set.go
@@ -2,7 +2,7 @@ package wechatpayopen
 
 // SubConfig 子商户配置
 func (c *Client) SubConfig(subAppid, subMchId string) *Client {
-	c.config.SpAppid = subAppid
+	c.config.SubAppid = subAppid
 	c.config.SubMchId = subMchId
 	return c
 }
