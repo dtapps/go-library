69e6bc38d7e7967862db28374d3461e61894a6ef
李光春 <gc@dtapp.net>
2023-04-01 12:03:14 +0800

    - update wechatopen

--- service/wechatopen/wxa.get_latest_auditstatus.go
+++ service/wechatopen/wxa.get_latest_auditstatus.go
@@ -9,12 +9,15 @@ import (
 )
 
 type WxaGetLatestAuditStatusResponse struct {
-	Errcode    int    `json:"errcode"`    // 返回码
-	Errmsg     string `json:"errmsg"`     // 错误信息
-	Auditid    int    `json:"auditid"`    // 最新的审核 ID
-	Status     int    `json:"status"`     // 审核状态
-	Reason     string `json:"reason"`     // 当审核被拒绝时，返回的拒绝原因
-	ScreenShot string `json:"ScreenShot"` // 当审核被拒绝时，会返回审核失败的小程序截图示例。用 | 分隔的 media_id 的列表，可通过获取永久素材接口拉取截图内容
+	Errcode         int    `json:"errcode"`           // 返回码
+	Errmsg          string `json:"errmsg"`            // 错误信息
+	Auditid         int    `json:"auditid"`           // 最新的审核 ID
+	Status          int    `json:"status"`            // 审核状态
+	Reason          string `json:"reason"`            // 当审核被拒绝时，返回的拒绝原因
+	ScreenShot      string `json:"ScreenShot"`        // 当审核被拒绝时，会返回审核失败的小程序截图示例。用 | 分隔的 media_id 的列表，可通过获取永久素材接口拉取截图内容
+	UserDesc        string `json:"user_desc"`         // 审核版本
+	UserVersion     string `json:"user_version"`      // 版本描述
+	SubmitAuditTime int64  `json:"submit_audit_time"` // 时间戳，提交审核的时间
 }
 
 type WxaGetLatestAuditStatusResult struct {
