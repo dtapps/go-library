8f236f48329e74a724d808468de6f3bb7656dae8
李光春 <gc@dtapp.net>
2023-06-04 10:13:09 +0800

    - update gostring

---
+++ utils/gostring/url.go
@@ -0,0 +1,35 @@
+package gostring
+
+import (
+	"strings"
+)
+
+// CompleteUrlHttp 补全 URL
+func CompleteUrlHttp(url string) string {
+	if strings.HasPrefix(url, "//") {
+		url = "http:" + url
+	} else if strings.HasPrefix(url, "://") {
+		url = "http" + url
+	} else if strings.HasPrefix(url, "http://") {
+	} else if strings.HasPrefix(url, "https://") {
+		url = Replace(url, "https://", "http://")
+	} else {
+		url = "http://" + url
+	}
+	return url
+}
+
+// CompleteUrlHttps 补全 URL
+func CompleteUrlHttps(url string) string {
+	if strings.HasPrefix(url, "//") {
+		url = "https:" + url
+	} else if strings.HasPrefix(url, "://") {
+		url = "https" + url
+	} else if strings.HasPrefix(url, "http://") {
+		url = Replace(url, "http://", "https://")
+	} else if strings.HasPrefix(url, "https://") {
+	} else {
+		url = "https://" + url
+	}
+	return url
+}
