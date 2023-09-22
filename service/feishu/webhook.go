398344f03a5e56c56bce3d0e5622173f4c350a5f
dtapps <li.gc@icloud.com>
2023-09-22 15:49:01 +0800

    - update feishu

--- service/feishu/webhook.go
+++ service/feishu/webhook.go
@@ -19,21 +19,23 @@ type WebhookSendResult struct {
 	Result WebhookSendResponse // 结果
 	Body   []byte              // 内容
 	Http   gorequest.Response  // 请求
-	Err    error               // 错误
 }
 
-func newWebhookSendResult(result WebhookSendResponse, body []byte, http gorequest.Response, err error) *WebhookSendResult {
-	return &WebhookSendResult{Result: result, Body: body, Http: http, Err: err}
+func newWebhookSendResult(result WebhookSendResponse, body []byte, http gorequest.Response) *WebhookSendResult {
+	return &WebhookSendResult{Result: result, Body: body, Http: http}
 }
 
 // WebhookSend https://open.feishu.cn/document/ukTMukTMukTM/ucTM5YjL3ETO24yNxkjN
-func (c *Client) WebhookSend(ctx context.Context, notMustParams ...*gorequest.Params) *WebhookSendResult {
+func (c *Client) WebhookSend(ctx context.Context, notMustParams ...*gorequest.Params) (*WebhookSendResult, error) {
 	// 参数
 	params := gorequest.NewParamsWith(notMustParams...)
 	// 请求
 	request, err := c.request(ctx, apiUrl+fmt.Sprintf("/open-apis/bot/v2/hook/%s", c.GetKey()), params)
+	if err != nil {
+		return newWebhookSendResult(WebhookSendResponse{}, request.ResponseBody, request), err
+	}
 	// 定义
 	var response WebhookSendResponse
 	err = gojson.Unmarshal(request.ResponseBody, &response)
-	return newWebhookSendResult(response, request.ResponseBody, request, err)
+	return newWebhookSendResult(response, request.ResponseBody, request), err
 }
