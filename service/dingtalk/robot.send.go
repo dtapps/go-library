7206c89d7b7cc8dd4eba79cd048516dc1c987b12
dtapps <li.gc@icloud.com>
2023-09-22 15:48:14 +0800

    - update dingtalk

--- service/dingtalk/robot.send.go
+++ service/dingtalk/robot.send.go
@@ -18,24 +18,26 @@ type RobotSendResult struct {
 	Result RobotSendResponse  // 结果
 	Body   []byte             // 内容
 	Http   gorequest.Response // 请求
-	Err    error              // 错误
 }
 
-func newRobotSendResult(result RobotSendResponse, body []byte, http gorequest.Response, err error) *RobotSendResult {
-	return &RobotSendResult{Result: result, Body: body, Http: http, Err: err}
+func newRobotSendResult(result RobotSendResponse, body []byte, http gorequest.Response) *RobotSendResult {
+	return &RobotSendResult{Result: result, Body: body, Http: http}
 }
 
 // RobotSend 自定义机器人
 // https://open.dingtalk.com/document/group/custom-robot-access
-func (c *Client) RobotSend(ctx context.Context, notMustParams ...*gorequest.Params) *RobotSendResult {
+func (c *Client) RobotSend(ctx context.Context, notMustParams ...*gorequest.Params) (*RobotSendResult, error) {
 	// 参数
 	params := gorequest.NewParamsWith(notMustParams...)
 	// 时间
 	timestamp := time.Now().UnixNano() / 1e6
 	// 请求
 	request, err := c.request(ctx, apiUrl+fmt.Sprintf("/robot/send?access_token=%s&timestamp=%d&sign=%s", c.GetAccessToken(), timestamp, c.sign(timestamp)), params, http.MethodPost)
+	if err != nil {
+		return newRobotSendResult(RobotSendResponse{}, request.ResponseBody, request), err
+	}
 	// 定义
 	var response RobotSendResponse
 	err = gojson.Unmarshal(request.ResponseBody, &response)
-	return newRobotSendResult(response, request.ResponseBody, request, err)
+	return newRobotSendResult(response, request.ResponseBody, request), err
 }
