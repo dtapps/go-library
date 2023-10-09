43ac9db2810f5f73a6b372df251bbbda22b59669
dtapps <li.gc@icloud.com>
2023-10-09 17:59:30 +0800

    - update

--- library.go
+++ library.go
@@ -1,5 +1,5 @@
 package go_library
 
 func Version() string {
-	return "1.0.199"
+	return "1.0.200"
 }
--- utils/gojobs/model.go
+++ utils/gojobs/model.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/dtapps/go-library/utils/gojobs/jobs_gorm_model"
 	"github.com/dtapps/go-library/utils/gojson"
-	"time"
 )
 
 // 创建模型
@@ -20,25 +19,22 @@ func (c *Client) autoMigrateTask(ctx context.Context) {
 
 // TaskLog 任务日志模型
 type TaskLog struct {
-	LogId            uint      `json:"log_id"`             // 日志编号
-	TaskId           uint      `json:"task_id"`            // 任务编号
-	TaskRunId        string    `json:"task_run_id"`        // 执行编号
-	TaskResultStatus string    `json:"task_result_status"` // 执行状态
-	TaskResultCode   int       `json:"task_result_code"`   // 执行状态码
-	TaskResultDesc   string    `json:"task_result_desc"`   // 执行结果
-	SystemHostName   string    `json:"system_host_name"`   // 主机名
-	SystemInsideIp   string    `json:"system_inside_ip"`   // 内网ip
-	SystemOs         string    `json:"system_os"`          // 系统类型
-	SystemArch       string    `json:"system_arch"`        // 系统架构
-	GoVersion        string    `json:"go_version"`         // go版本
-	SdkVersion       string    `json:"sdk_version"`        // sdk版本
-	SystemOutsideIp  string    `json:"system_outside_ip"`  // 外网ip
-	LogTime          time.Time `json:"log_time"`           // 日志时间
+	TaskId           uint   `json:"task_id"`            // 任务编号
+	TaskRunId        string `json:"task_run_id"`        // 执行编号
+	TaskResultStatus string `json:"task_result_status"` // 执行状态
+	TaskResultCode   int    `json:"task_result_code"`   // 执行状态码
+	TaskResultDesc   string `json:"task_result_desc"`   // 执行结果
+	SystemHostName   string `json:"system_host_name"`   // 主机名
+	SystemInsideIp   string `json:"system_inside_ip"`   // 内网ip
+	SystemOs         string `json:"system_os"`          // 系统类型
+	SystemArch       string `json:"system_arch"`        // 系统架构
+	GoVersion        string `json:"go_version"`         // go版本
+	SdkVersion       string `json:"sdk_version"`        // sdk版本
+	SystemOutsideIp  string `json:"system_outside_ip"`  // 外网ip
 }
 
 // TaskLogRecord 记录
 func (c *Client) TaskLogRecord(ctx context.Context, task jobs_gorm_model.Task, runId string, taskResultCode int, taskResultDesc string) {
-
 	if c.runSlog.status {
 		taskLog := TaskLog{
 			TaskId:          task.Id,
