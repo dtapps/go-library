b68f04ce2727c3186a44e44fc0b62e1d962b9378
dtapps <li.gc@icloud.com>
2023-11-11 11:37:08 +0800

    - add dorm ent

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/dtapps/go-library
 go 1.20
 
 require (
+	entgo.io/ent v0.12.4
 	gitee.com/chunanyong/zorm v1.7.3
 	github.com/MercuryEngineering/CookieMonster v0.0.0-20180304172713-1584578b3403
 	github.com/aliyun/aliyun-oss-go-sdk v3.0.1+incompatible
@@ -29,6 +30,7 @@ require (
 	github.com/shirou/gopsutil v3.21.11+incompatible
 	github.com/sirupsen/logrus v1.9.3
 	github.com/tencentyun/cos-go-sdk-v5 v0.7.45
+	github.com/upper/db/v4 v4.7.0
 	github.com/uptrace/bun v1.1.16
 	github.com/uptrace/bun/dialect/mysqldialect v1.1.16
 	github.com/uptrace/bun/dialect/pgdialect v1.1.16
@@ -48,6 +50,9 @@ require (
 )
 
 require (
+	ariga.io/atlas v0.15.0 // indirect
+	github.com/agext/levenshtein v1.2.3 // indirect
+	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/bitly/go-simplejson v0.5.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
@@ -57,10 +62,20 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/go-openapi/inflect v0.19.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.19.1 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx/v4 v4.18.1 // indirect
 	github.com/jackc/pgx/v5 v5.5.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -70,6 +85,7 @@ require (
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.18 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
@@ -79,6 +95,7 @@ require (
 	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/saracen/go7z-fixtures v0.0.0-20190623165746-aa6b8fba1d2f // indirect
 	github.com/saracen/solidblock v0.0.0-20190426153529-45df20abab6f // indirect
+	github.com/segmentio/fasthash v1.0.3 // indirect
 	github.com/syndtr/goleveldb v1.0.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
@@ -93,6 +110,7 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	github.com/zclconf/go-cty v1.14.1 // indirect
 	golang.org/x/arch v0.6.0 // indirect
 	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.18.0 // indirect
---
+++ vendor/ariga.io/atlas/sql/postgres/driver_oss.go
@@ -0,0 +1,197 @@
+// Copyright 2021-present The Atlas Authors. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+//go:build !ent
+
+package postgres
+
+import (
+	"context"
+	"fmt"
+
+	"ariga.io/atlas/schemahcl"
+	"ariga.io/atlas/sql/internal/specutil"
+	"ariga.io/atlas/sql/internal/sqlx"
+	"ariga.io/atlas/sql/migrate"
+	"ariga.io/atlas/sql/schema"
+	"ariga.io/atlas/sql/sqlspec"
+)
+
+var (
+	specOptions []schemahcl.Option
+	specFuncs   = &specutil.Funcs{
+		Table: tableSpec,
+		View:  viewSpec,
+	}
+	scanFuncs = &specutil.ScanFuncs{
+		Table: convertTable,
+		View:  convertView,
+	}
+)
+
+func (*inspect) inspectViews(context.Context, *schema.Realm, *schema.InspectOptions) error {
+	return nil // unimplemented.
+}
+
+func (*inspect) inspectFuncs(context.Context, *schema.Realm, *schema.InspectOptions) error {
+	return nil // unimplemented.
+}
+
+func (i *inspect) inspectTypes(context.Context, *schema.Realm, *schema.InspectOptions) error {
+	return nil // unimplemented.
+}
+
+func (*state) addView(*schema.AddView) error {
+	return nil // unimplemented.
+}
+
+func (*state) dropView(*schema.DropView) error {
+	return nil // unimplemented.
+}
+
+func (*state) modifyView(*schema.ModifyView) error {
+	return nil // unimplemented.
+}
+
+func (*state) renameView(*schema.RenameView) {
+	// unimplemented.
+}
+
+func (s *state) addFunc(*schema.AddFunc) error {
+	return nil // unimplemented.
+}
+
+func (s *state) dropFunc(*schema.DropFunc) error {
+	return nil // unimplemented.
+}
+
+func (s *state) modifyFunc(*schema.ModifyFunc) error {
+	return nil // unimplemented.
+}
+
+func (s *state) renameFunc(*schema.RenameFunc) error {
+	return nil // unimplemented.
+}
+
+func (s *state) addProc(*schema.AddProc) error {
+	return nil // unimplemented.
+}
+
+func (s *state) dropProc(*schema.DropProc) error {
+	return nil // unimplemented.
+}
+
+func (s *state) modifyProc(*schema.ModifyProc) error {
+	return nil // unimplemented.
+}
+
+func (s *state) renameProc(*schema.RenameProc) error {
+	return nil // unimplemented.
+}
+
+func (s *state) addObject(add *schema.AddObject) error {
+	switch o := add.O.(type) {
+	case *schema.EnumType:
+		create, drop := s.createDropEnum(o)
+		s.append(&migrate.Change{
+			Source:  add,
+			Cmd:     create,
+			Reverse: drop,
+			Comment: fmt.Sprintf("create enum type %q", o.T),
+		})
+	default:
+		// unsupported object type.
+	}
+	return nil
+}
+
+func (s *state) dropObject(drop *schema.DropObject) error {
+	switch o := drop.O.(type) {
+	case *schema.EnumType:
+		create, dropE := s.createDropEnum(o)
+		s.append(&migrate.Change{
+			Source:  drop,
+			Cmd:     dropE,
+			Reverse: create,
+			Comment: fmt.Sprintf("drop enum type %q", o.T),
+		})
+	default:
+		// unsupported object type.
+	}
+	return nil
+}
+
+func (s *state) modifyObject(modify *schema.ModifyObject) error {
+	if _, ok := modify.From.(*schema.EnumType); ok {
+		return s.alterEnum(modify)
+	}
+	return nil // unimplemented.
+}
+
+func (d *diff) ViewAttrChanged(_, _ *schema.View) bool {
+	return false // unimplemented.
+}
+
+// SchemaObjectDiff returns a changeset for migrating schema objects from
+// one state to the other.
+func (*diff) SchemaObjectDiff(from, to *schema.Schema) ([]schema.Change, error) {
+	var changes []schema.Change
+	// Drop or modify enums.
+	for _, o1 := range from.Objects {
+		e1, ok := o1.(*schema.EnumType)
+		if !ok {
+			continue // Unsupported object type.
+		}
+		o2, ok := to.Object(func(o schema.Object) bool {
+			e2, ok := o.(*schema.EnumType)
+			return ok && e1.T == e2.T
+		})
+		if !ok {
+			changes = append(changes, &schema.DropObject{O: o1})
+			continue
+		}
+		if e2 := o2.(*schema.EnumType); !sqlx.ValuesEqual(e1.Values, e2.Values) {
+			changes = append(changes, &schema.ModifyObject{From: e1, To: e2})
+		}
+	}
+	// Add new enums.
+	for _, o1 := range to.Objects {
+		e1, ok := o1.(*schema.EnumType)
+		if !ok {
+			continue // Unsupported object type.
+		}
+		if _, ok := from.Object(func(o schema.Object) bool {
+			e2, ok := o.(*schema.EnumType)
+			return ok && e1.T == e2.T
+		}); !ok {
+			changes = append(changes, &schema.AddObject{O: e1})
+		}
+	}
+	return changes, nil
+}
+
+func verifyChanges(context.Context, []schema.Change) error {
+	return nil // unimplemented.
+}
+
+func convertDomains(_ []*sqlspec.Table, domains []*Domain, _ *schema.Realm) error {
+	if len(domains) > 0 {
+		return fmt.Errorf("postgres: domains are not supported by this version. Use: https://atlasgo.io/getting-started")
+	}
+	return nil
+}
+
+// objectSpec converts from a concrete schema objects into specs.
+func objectSpec(d *doc, spec *specutil.SchemaSpec, s *schema.Schema) error {
+	for _, o := range s.Objects {
+		if e, ok := o.(*schema.EnumType); ok {
+			d.Enums = append(d.Enums, &Enum{
+				Name:   e.T,
+				Values: e.Values,
+				Schema: specutil.SchemaRef(spec.Schema.Name),
+			})
+		}
+	}
+	return nil
+}
