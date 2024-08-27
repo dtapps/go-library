f5e8d5e1ef2801983407d79690fcd9d44698c130
dtapps <li.gc@icloud.com>
2024-08-27 15:07:54 +0800

    - update contrib/hertz-requestid/v1.0.2

--- contrib/hertz-requestid/middleware.go
+++ contrib/hertz-requestid/middleware.go
@@ -4,9 +4,20 @@ import (
 	"context"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/google/uuid"
+	"github.com/hertz-contrib/requestid"
 )
 
 var HeaderXRequestID = "X-Request-ID"
+
+// NewKj 初始化RequestID中间件
+func NewKj(opts ...requestid.Option) app.HandlerFunc {
+	return requestid.New(opts...)
+}
+
+// GetKj 返回请求标识符
+func GetKj(c *app.RequestContext) string {
+	return requestid.Get(c)
+}
 
 // New 初始化RequestID中间件
 func New() app.HandlerFunc {
@@ -17,10 +28,13 @@ func New() app.HandlerFunc {
 		if requestId == "" {
 			requestId = uuid.New().String()
 		}
+
+		// 设置id
 		c.Set(HeaderXRequestID, requestId)
-		// 设置id以确保请求id在响应中
 		c.Header(HeaderXRequestID, requestId)
 		ctx = context.WithValue(ctx, HeaderXRequestID, requestId)
+
+		// 继续
 		c.Next(ctx)
 	}
 }
