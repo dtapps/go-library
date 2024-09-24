4800ec50a7530a391611f04d1340775628d426d3
dtapps <li.gc@icloud.com>
2024-09-24 12:30:48 +0800

    - update service/wechatopen/v1.0.60

--- service/wechatopen/cgi-bin.get_api_domain_ip.go
+++ service/wechatopen/cgi-bin.get_api_domain_ip.go
@@ -6,23 +6,23 @@ import (
 	"net/http"
 )
 
-type GetCallBackIpResponse struct {
+type CgiBinGetApiDomainIpResponse struct {
 	IpList []string `json:"ip_list,omitempty"`
 }
 
-type GetCallBackIpResult struct {
-	Result GetCallBackIpResponse // 结果
-	Body   []byte                // 内容
-	Http   gorequest.Response    // 请求
+type CgiBinGetApiDomainIpResult struct {
+	Result CgiBinGetApiDomainIpResponse // 结果
+	Body   []byte                       // 内容
+	Http   gorequest.Response           // 请求
 }
 
-func NewGetCallBackIpResult(result GetCallBackIpResponse, body []byte, http gorequest.Response) *GetCallBackIpResult {
-	return &GetCallBackIpResult{Result: result, Body: body, Http: http}
+func NewCgiBinGetApiDomainIpResult(result CgiBinGetApiDomainIpResponse, body []byte, http gorequest.Response) *CgiBinGetApiDomainIpResult {
+	return &CgiBinGetApiDomainIpResult{Result: result, Body: body, Http: http}
 }
 
 // CgiBinGetApiDomainIp 获取微信API接口 IP地址
 // https://developers.weixin.qq.com/doc/offiaccount/Basic_Information/Get_the_WeChat_server_IP_address.html
-func (c *Client) CgiBinGetApiDomainIp(ctx context.Context, componentAccessToken string, notMustParams ...gorequest.Params) (*GetCallBackIpResult, error) {
+func (c *Client) CgiBinGetApiDomainIp(ctx context.Context, componentAccessToken string, notMustParams ...gorequest.Params) (*CgiBinGetApiDomainIpResult, error) {
 
 	// OpenTelemetry链路追踪
 	ctx, span := TraceStartSpan(ctx, "cgi-bin/get_api_domain_ip")
@@ -32,7 +32,7 @@ func (c *Client) CgiBinGetApiDomainIp(ctx context.Context, componentAccessToken 
 	params := gorequest.NewParamsWith(notMustParams...)
 
 	// 请求
-	var response GetCallBackIpResponse
+	var response CgiBinGetApiDomainIpResponse
 	request, err := c.request(ctx, span, "cgi-bin/get_api_domain_ip?access_token="+componentAccessToken, params, http.MethodGet, &response)
-	return NewGetCallBackIpResult(response, request.ResponseBody, request), err
+	return NewCgiBinGetApiDomainIpResult(response, request.ResponseBody, request), err
 }
--- service/wechatopen/version.go
+++ service/wechatopen/version.go
@@ -1,5 +1,5 @@
 package wechatopen
 
 const (
-	Version = "1.0.59"
+	Version = "1.0.60"
 )
