5a6989da37688d7ca5cb14d64488d48d2f9a9633
dtapps <li.gc@icloud.com>
2024-12-10 15:01:26 +0800

    - update utils/gojobs/v1.0.183

--- utils/gojobs/ent_model.go
+++ utils/gojobs/ent_model.go
@@ -25,7 +25,7 @@ func EntTaskFields() []ent.Field {
 		field.Int64("id").Comment("记录编号").Annotations(entsql.WithComments(true)),
 		field.String("status").Optional().Comment("状态码").Annotations(entsql.WithComments(true)),
 		field.String("status_desc").Optional().Comment("状态描述").Annotations(entsql.WithComments(true)),
-		field.String("spec").NotEmpty().Immutable().Comment("任务规则").Annotations(entsql.WithComments(true)),
+		field.String("spec").NotEmpty().Comment("任务规则").Annotations(entsql.WithComments(true)),
 		field.String("params").Optional().Comment("参数").Annotations(entsql.WithComments(true)),
 		field.Int64("frequency").Optional().Immutable().Comment("频率(秒单位)").Annotations(entsql.WithComments(true)),
 		field.Int64("number").Optional().Default(0).Comment("当前次数").Annotations(entsql.WithComments(true)),
