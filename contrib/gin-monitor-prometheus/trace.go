a369961dc625e13da8b933c32453e0fed6f05358
dtapps <li.gc@icloud.com>
2025-03-19 14:45:36 +0800

    - add contrib/gin-monitor-prometheus/v1.0.0

---
+++ contrib/gin-monitor-prometheus/trace.go
@@ -0,0 +1,106 @@
+package gin_monitor_prometheus
+
+import (
+	"github.com/gin-gonic/gin"
+	prom "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	labelMethod     = "method"
+	labelStatusCode = "statusCode"
+	labelPath       = "path"
+
+	unknownLabelValue = "unknown"
+)
+
+// genLabels make labels values.
+func genLabels(c *gin.Context) prom.Labels {
+	labels := make(prom.Labels)
+	labels[labelMethod] = defaultValIfEmpty(c.Request.Method, unknownLabelValue)
+	labels[labelStatusCode] = defaultValIfEmpty(strconv.Itoa(c.Writer.Status()), unknownLabelValue)
+	labels[labelPath] = defaultValIfEmpty(c.FullPath(), unknownLabelValue)
+
+	return labels
+}
+
+type ServerTracer struct {
+	serverHandledCounter   *prom.CounterVec
+	serverHandledHistogram *prom.HistogramVec
+}
+
+func (s *ServerTracer) Middleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+
+		// 处理请求
+		c.Next()
+
+		cost := time.Now().Sub(start)
+		_ = counterAdd(s.serverHandledCounter, 1, genLabels(c))
+		_ = histogramObserve(s.serverHandledHistogram, cost, genLabels(c))
+
+	}
+}
+
+// NewServerTracer provides tracer for server access, addr and path is the scrape_configs for prometheus server.
+func NewServerTracer(addr, path string, opts ...Option) *ServerTracer {
+	cfg := defaultConfig()
+
+	for _, opts := range opts {
+		opts.apply(cfg)
+	}
+
+	if !cfg.disableServer {
+		httpServer := http.DefaultServeMux
+		if !cfg.useDefaultServerMux {
+			httpServer = http.NewServeMux()
+		}
+		httpServer.Handle(path, promhttp.HandlerFor(cfg.registry, promhttp.HandlerOpts{ErrorHandling: promhttp.ContinueOnError}))
+		go func() {
+			if err := http.ListenAndServe(addr, httpServer); err != nil {
+				log.Fatal("GIN: Unable to start a promhttp server, err: " + err.Error())
+			}
+		}()
+	}
+
+	serverHandledCounter := prom.NewCounterVec(
+		prom.CounterOpts{
+			Name: "gin_server_throughput",
+			Help: "Total number of HTTPs completed by the server, regardless of success or failure.",
+		},
+		[]string{labelMethod, labelStatusCode, labelPath},
+	)
+	cfg.registry.MustRegister(serverHandledCounter)
+
+	serverHandledHistogram := prom.NewHistogramVec(
+		prom.HistogramOpts{
+			Name:    "gin_server_latency_us",
+			Help:    "Latency (microseconds) of HTTP that had been application-level handled by the server.",
+			Buckets: cfg.buckets,
+		},
+		[]string{labelMethod, labelStatusCode, labelPath},
+	)
+	cfg.registry.MustRegister(serverHandledHistogram)
+
+	if cfg.enableGoCollector {
+		cfg.registry.MustRegister(collectors.NewGoCollector(collectors.WithGoCollectorRuntimeMetrics(cfg.runtimeMetricRules...)))
+	}
+
+	return &ServerTracer{
+		serverHandledCounter:   serverHandledCounter,
+		serverHandledHistogram: serverHandledHistogram,
+	}
+}
+
+func defaultValIfEmpty(val, def string) string {
+	if val == "" {
+		return def
+	}
+	return val
+}
---
+++ contrib/gin-monitor-prometheus/version.go
@@ -0,0 +1,5 @@
+package gin_monitor_prometheus
+
+const (
+	Version = "1.0.0"
+)
