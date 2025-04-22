f61ea7365c9c1ad1dc2c95753c25f6ea3375bba7
dtapps <li.gc@icloud.com>
2025-04-22 11:54:23 +0800

    - update service/wechatopen/v1.0.66

--- service/wechatopen/request.go
+++ service/wechatopen/request.go
@@ -2,21 +2,31 @@ package wechatopen
 
 import (
 	"context"
+	"fmt"
 	"go.dtapp.net/library/utils/gorequest"
-	"net/http"
 )
 
 type RequestAuthorizerParams struct {
 	Url         string
+	UrlMethod   string
 	AccessToken string
 	Response    any
 }
 
 // RequestAuthorizer 发起小程序
 func (c *Client) RequestAuthorizer(ctx context.Context, param RequestAuthorizerParams, notMustParams ...*gorequest.Params) (gorequest.Response, error) {
+	if param.Url == "" {
+		return gorequest.Response{}, fmt.Errorf("url is empty")
+	}
+	if param.UrlMethod == "" {
+		return gorequest.Response{}, fmt.Errorf("url_method is empty")
+	}
+	if param.AccessToken == "" {
+		return gorequest.Response{}, fmt.Errorf("access_token is empty")
+	}
 	// 参数
 	params := gorequest.NewParamsWith(notMustParams...)
 	// 请求
-	request, err := c.request(ctx, param.Url+"?access_token="+param.AccessToken, params, http.MethodPost, &param.Response)
+	request, err := c.request(ctx, param.Url+"?access_token="+param.AccessToken, params, param.UrlMethod, &param.Response)
 	return request, err
 }
--- service/wechatopen/version.go
+++ service/wechatopen/version.go
@@ -1,5 +1,5 @@
 package wechatopen
 
 const (
-	Version = "1.0.65"
+	Version = "1.0.66"
 )
