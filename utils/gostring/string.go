44bd11a3b518c90038f2abb2913d3d1c455278c7
dtapps <li.gc@icloud.com>
2025-04-24 15:26:18 +0800

    feat(gostring): 添加EqualSlices函数并更新版本号
    新增EqualSlices函数用于比较两个字符串切片是否相等，同时将版本号更新至1.0.27

--- utils/gostring/string.go
+++ utils/gostring/string.go
@@ -279,3 +279,16 @@ func GetQueryParamSimple(urlStr, paramName string) string {
 
 	return ""
 }
+
+// EqualSlices 比较两个字符串切片是否相等
+func EqualSlices(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
--- utils/gostring/version.go
+++ utils/gostring/version.go
@@ -1,3 +1,3 @@
 package gostring
 
-const Version = "1.0.26"
+const Version = "1.0.27"
