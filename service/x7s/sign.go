ce31ce5c3f9c754fc3b9673f56cf38100fb77004
dtapps <li.gc@icloud.com>
2025-04-27 12:30:49 +0800

    - update service/x7s/v1.0.11

--- service/x7s/sign.go
+++ service/x7s/sign.go
@@ -2,6 +2,7 @@ package x7s
 
 import (
 	"fmt"
+	"github.com/spf13/cast"
 	"go.dtapp.net/library/utils/gomd5"
 	"go.dtapp.net/library/utils/gorequest"
 	"log"
@@ -11,13 +12,13 @@ import (
 // 签名
 func (c *Client) sign(param *gorequest.Params) string {
 	var keys []string
-	for k := range param.DeepGet() {
+	for k := range param.DeepGetAny() {
 		keys = append(keys, k)
 	}
 	sort.Strings(keys)
 	signStr := ""
 	for _, key := range keys {
-		signStr += fmt.Sprintf("%s=%s&", key, fmt.Sprint(key))
+		signStr += fmt.Sprintf("%s=%s&", key, cast.ToString(key))
 	}
 	signStr += fmt.Sprintf("key=%s", c.GetApiKey())
 	log.Println(signStr)
--- service/x7s/version.go
+++ service/x7s/version.go
@@ -1,5 +1,5 @@
 package x7s
 
 const (
-	Version = "1.0.10"
+	Version = "1.0.11"
 )
