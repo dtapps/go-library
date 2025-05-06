50139147ad155b87fd6d734fad4847193a8ec976
dtapps <li.gc@icloud.com>
2025-05-06 13:42:18 +0800

    - update utils/gorequest/v1.0.96

---
+++ utils/gorequest/string.go
@@ -0,0 +1,23 @@
+package gorequest
+
+import (
+	"encoding/json"
+	"strconv"
+)
+
+// GetString 获取字符串
+func GetString(i any) string {
+	switch v := i.(type) {
+	case string:
+		return v
+	case []byte:
+		return string(v)
+	case int:
+		return strconv.Itoa(v)
+	case bool:
+		return strconv.FormatBool(v)
+	default:
+		bytes, _ := json.Marshal(v)
+		return string(bytes)
+	}
+}
--- utils/gorequest/version.go
+++ utils/gorequest/version.go
@@ -1,3 +1,3 @@
 package gorequest
 
-const Version = "1.0.95"
+const Version = "1.0.96"
