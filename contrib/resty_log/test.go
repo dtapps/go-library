43b906cbc1559ea84bd656d3ea98f3526a83f63a
dtapps <li.gc@icloud.com>
2025-09-28 23:51:12 +0800

    feat(resty_log): 添加测试日志实现并更新版本号
    新增测试用的日志保存实现和回调函数，同时将版本号更新至1.0.1。

---
+++ contrib/resty_log/test.go
@@ -0,0 +1,46 @@
+package resty_log
+
+import (
+	"context"
+	"log/slog"
+)
+
+// 实现 LogSaver 接口
+type TestEntLogSaver struct{}
+
+func (s *TestEntLogSaver) SaveLog(ctx context.Context, data *LogData) error {
+	slog.Info("[接口方式]准备保存日志")
+	if data == nil {
+		return nil
+	}
+	slog.Info("[接口方式]保存日志",
+		slog.String("Hostname", data.Hostname),
+		slog.String("Method", data.Method),
+		slog.String("URL", data.URL),
+	)
+	return nil
+}
+
+// 返回一个测试的 Logger
+func NewTestLogger(debug bool) *Logger {
+
+	// 定义回调函数
+	saveFunc := func(ctx context.Context, data *LogData) error {
+		slog.Info("[回调方式]准备保存日志")
+		if data == nil {
+			return nil
+		}
+		slog.Info("[回调方式]保存日志",
+			slog.String("Hostname", data.Hostname),
+			slog.String("Method", data.Method),
+			slog.String("URL", data.URL),
+		)
+		return nil
+	}
+
+	// 创建 LogSaver 实现
+	saver := &TestEntLogSaver{}
+
+	// 初始化 Logger，传入回调和接口（回调优先）
+	return NewLogger(saveFunc, saver, debug)
+}
--- contrib/resty_log/version.go
+++ contrib/resty_log/version.go
@@ -1,4 +1,4 @@
 package resty_log
 
 // Version 版本
-const Version = "1.0.0"
+const Version = "1.0.1"
